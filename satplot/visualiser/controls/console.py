e8aeb969f194ac1aa8c38f023b0cc9210617f06a
Robert Mearns <robert.mearns@unimelb.edu.au>
2024-02-27 17:41:15 +1100

    adding line overwrite if a carriage return with no newline in the previous line

--- satplot/visualiser/controls/console.py
+++ satplot/visualiser/controls/console.py
@@ -1,16 +1,27 @@
 from PyQt5 import QtWidgets, QtCore, QtGui
-import html
+import sys
+import string
+
 
 consolefp = None
+printable = string.ascii_letters + string.digits + string.punctuation + ' '
+
 
 def send(str):
 	print(str, file=consolefp)
-	# consolefp.flush()
+
+def hex_escape(s):
+    return ''.join(c if c in printable else r'\x{0:02x}'.format(ord(c)) for c in s)
 
 class EmittingConsoleStream(QtCore.QObject):
 	textWritten = QtCore.pyqtSignal(str)
 
+	# called on print in send function above
 	def write(self, text):
+		# if "\n" in text:
+		# 	print(f"newline is in text: {text}")
+		# else:
+		# 	print(f"newline is not in text: {text}")
 		self.textWritten.emit(str(text))
 
 class Console(QtWidgets.QWidget):
@@ -20,7 +31,7 @@ class Console(QtWidgets.QWidget):
 
 		self.errCol = QtGui.QColor(255,0,0)
 		self.stdCol = QtGui.QColor(51,255,0)
-
+		self.overwriting = False
 		layout = QtWidgets.QVBoxLayout()
 		self.text_box = QtWidgets.QTextEdit()
 		layout.addWidget(self.text_box)
@@ -30,19 +41,47 @@ class Console(QtWidgets.QWidget):
 			QTextEdit#console {
 			  	background-color: #000000}
 							  ''')
-		self.text_box.setReadOnly(True)
+		self.text_box.setReadOnly(False)
 
 	def writeOutput(self, text):
+		if text.isspace():
+			return
+		
 		cursor = self.text_box.textCursor()
 		self.text_box.setTextColor(self.stdCol)
-		self.text_box.insertPlainText(text)
-		cursor.movePosition(QtGui.QTextCursor.End)
+		
+
+		# hack to get colour to work for individual lines
+		self.text_box.insertPlainText(' ')
+
 		self.text_box.setTextCursor(cursor)
+		
+		if self.overwriting:
+			cursor.movePosition(QtGui.QTextCursor.StartOfLine, cursor.KeepAnchor)
+			cursor.movePosition(QtGui.QTextCursor.Up, cursor.KeepAnchor)
+			cursor.insertText(' ')
+
+		cursor.insertText(text+'\n')
+		cursor.movePosition(QtGui.QTextCursor.End)
+		if '\r\n' not in text and '\r' in text:
+			self.setOverwriteMode(True)
+		else:
+			self.setOverwriteMode(False)
+
+	def setOverwriteMode(self, state):
+		self.overwriting = state
+		self.text_box.setOverwriteMode(state)
 
 	def writeErr(self, text):
 		cursor = self.text_box.textCursor()
 		self.text_box.setTextColor(self.errCol)
-		self.text_box.insertPlainText(text)
-		# TODO: add cursor to end
+		# hack to get colour to work for individual lines
+		self.text_box.insertPlainText(' ')
+		
+		if self.overwriting:
+			self.setOverwriteMode(True)	
+		cursor.insertText(text)
+
 		cursor.movePosition(QtGui.QTextCursor.End)
+		self.setOverwriteMode(False)
 		self.text_box.setTextCursor(cursor)
