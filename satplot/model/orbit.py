7f3f747ffb08b1a468d3ee09e0e3684a5a95dd16
Robert Mearns <robert.mearns@unimelb.edu.au>
2024-06-27 10:47:06 +1000

    adding skyfield satrec object to satplot.orbit class; sometimes doesn't work, but failure conditions not really known at this stage; wrapped inside a try except for now

--- satplot/model/orbit.py
+++ satplot/model/orbit.py
@@ -167,7 +167,11 @@ class Orbit(object):
 			self.period = 2 * np.pi / valid_sats[-1].model.no_kozai * 60
 			self.period_steps = int(self.period / self.timespan.time_step.total_seconds())
 			self.name = valid_sats[-1].name
-			# self.sat = valid_sats[-1]
+			try:
+				self.sat = valid_sats[-1]
+			except:
+				# TODO: figure out what the exact error is and cause of when valid_sats doesn't exist
+				pass
 
 		elif self.gen_type == 'FAKE_TLE':
 			satrec = args[1]
