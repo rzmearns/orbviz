fa5e1ffab869f6dc77c67b7f28fc5c80bef9359f
Robert Mearns <robert.mearns@unimelb.edu.au>
2025-02-13 00:04:40 +1100

    removing old style thread cleanup

--- satplot/model/data_models/base.py
+++ satplot/model/data_models/base.py
@@ -21,14 +21,6 @@ class BaseDataModel(QtCore.QObject):
 		self.config['is_data_valid'] = False
 
 	def updateConfig(self, param:str, val:Any) -> None:
-		"""[summary]
-
-		[description]
-
-		Args:
-			param ([type]): [description]
-			val ([type]): [description]
-		"""
 		if param not in self.config.keys():
 			raise ValueError(f"{param} not a valid configuration option for {self.config['data_type']}")
 		self._setConfig(param, val)
@@ -62,30 +54,3 @@ class BaseDataModel(QtCore.QObject):
 		print(f"Data Config for {self.getConfigValue('data_type')}")
 		for k,v in self.config.items():
 			print(f'\t{k}:{v}')
-
-	# def _setUpLoadWorkerThread(self):
-	# 	''' call this after setting up the data to load in the inheriting class
-	# 		ensure that load_worker has been created before calling this function
-	# 		call using super()._setUpLoadWorkerThread()'''
-	# 	if self.load_worker is None:
-	# 		raise ValueError('Must create load_worker before calling super()._setUpLoadWorkerThread()')
-	# 	self.load_worker_thread = QtCore.QThread(parent=self.window)
-	# 	# Move to new thread and setup signals
-	# 	self.load_worker.moveToThread(self.load_worker_thread)
-	# 	self.load_worker_thread.started.connect(self.load_worker.run)
-	# 	self.load_worker_thread.finished.connect(self.load_worker_thread.deleteLater)
-	# 	self.load_worker.finished.connect(self._cleanUpLoadWorkerThread)
-	# 	self.load_worker.finished.connect(self._updateDataSources)
-	# 	self.load_worker.finished.connect(self._updateControls)
-	# 	self.load_worker.error.connect(self._cleanUpErrorInLoadWorkerThread)
-
-	# def _cleanUpLoadWorkerThread(self):
-	# 	if self.load_worker_thread is not None:
-	# 		self.load_worker_thread.quit()
-	# 		self.load_worker_thread.deleteLater()
-	# 	self.load_worker_thread = None
-	# 	self.load_worker = None
-
-	# def _cleanUpErrorInLoadWorkerThread(self, err_str=None):
-	# 	print(f'Clean Up Load Worker Thread Error: {err_str}', file=sys.stderr)
-	# 	return self._cleanUpLoadWorkerThread()
