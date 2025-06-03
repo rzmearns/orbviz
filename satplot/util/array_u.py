cfc0e580d0a09b958bd8ea91e48b10486928a6c3
Robert Mearns <robert.mearns@unimelb.edu.au>
2025-06-03 21:56:56 +1000

    adding nan interpolation utility

--- satplot/util/array_u.py
+++ satplot/util/array_u.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numpy.typing as nptyping
+from scipy.interpolate import interp1d
 
 def uniqueRows(arr:nptyping.NDArray, to_decimal:int=8) -> nptyping.NDArray:
 	'''
@@ -36,6 +37,51 @@ def arrayOverlap(a:nptyping.NDArray, b:nptyping.NDArray) -> nptyping.NDArray:
 		[description]
 	'''
 	return np.array([x for x in set(tuple(x) for x in a) & set(tuple(x) for x in b)])
+
+def interpNans(y, x=None):
+	if x is None:
+		x = np.arange(len(y))
+	nans = np.isnan(y)
+
+	interpolator = interp1d(
+		x[~nans],
+		y[~nans],
+		kind="linear",
+		fill_value="extrapolate",
+		assume_sorted=True,
+	)
+
+	return interpolator(x)
+
+def nonMonotonicInterpNans(x, y):
+	out_y = y.copy()
+	# check if assumptions are met
+	if len(x) != len(out_y):
+		raise ValueError('Length of x and y arrays must be equal')
+	if np.isnan(x).any():
+		raise ValueError('x array cannot have any nans')
+
+	nans = np.where(np.isnan(y))[0]
+	# handle if first or last elements are nan
+	if 0 in nans:
+		out_y[0] = out_y[1]
+	if len(out_y)-1 in nans:
+		out_y[-1] = out_y[-2]
+
+	nans = np.where(np.isnan(out_y))[0]
+	if len(nans) == 0:
+		return out_y
+
+
+	# linear interpolate using just the neighbours of the nan idxs
+	ys = np.vstack((out_y[nans-1],np.vstack((out_y[nans],out_y[nans+1]))))
+	xs = np.vstack((x[nans-1],np.vstack((x[nans],x[nans+1]))))
+	new_ys = (ys[0,:]*(xs[2,:]-xs[1,:])+ys[2,:]*(xs[1,:]-xs[0,:]))/\
+	            (xs[2,:]-xs[0,:])
+
+	out_y[nans] = new_ys
+	return out_y
+
 
 def findRows(subarr:nptyping.NDArray, arr:nptyping.NDArray) -> list[int]:
 	'''Finds indices in an array row-wise
