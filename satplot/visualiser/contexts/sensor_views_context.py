73a49d93780d6224eed20ef36cb30e94b2be7a80
Robert Mearns <robert.mearns@unimelb.edu.au>
2025-06-11 19:38:01 +1000

    modifying sensor views context to have multiple data models stored in dict

--- satplot/visualiser/contexts/sensor_views_context.py
+++ satplot/visualiser/contexts/sensor_views_context.py
@@ -1,8 +1,10 @@
 import logging
 from PyQt5 import QtWidgets, QtCore
+from typing import Any
 
 import satplot.model.data_models.data_types as data_types
 from satplot.model.data_models.history_data import HistoryData
+from satplot.model.data_models.earth_raycast_data import (EarthRayCastData)
 from satplot.visualiser.contexts.base_context import (BaseContext, BaseControls)
 from satplot.visualiser.contexts.canvas_wrappers.base_cw import (BaseCanvas)
 import satplot.visualiser.contexts.canvas_wrappers.sensor_views_cw as sensor_views_cw
@@ -13,15 +15,18 @@ import satplot.visualiser.interface.console as console
 logger = logging.getLogger(__name__)
 
 class SensorViewsContext(BaseContext):
-	data_type = data_types.DataType.HISTORY
-	def __init__(self, name:str, parent_window:QtWidgets.QMainWindow, data:HistoryData):
+	data_type = [data_types.DataType.HISTORY,
+				data_types.DataType.PLANETARYRAYCAST]
+	def __init__(self, name:str, parent_window:QtWidgets.QMainWindow, history_data:HistoryData, raycast_data:EarthRayCastData):
 		super().__init__(name)
 		self.window = parent_window
+		self.data: dict[str,Any] = {}
+		self.data['history'] = history_data
+		self.data['raycast_src'] = raycast_data
 		self._validateDataType()
-		self.data = data
 
 		self.canvas_wrapper = sensor_views_cw.SensorViewsCanvasWrapper()
-		self.canvas_wrapper.setModel(self.data)
+		self.canvas_wrapper.setModel(self.data['history'], self.data['raycast_src'])
 		self.controls = Controls(self, self.canvas_wrapper)
 
 		disp_hsplitter = QtWidgets.QSplitter(QtCore.Qt.Orientation.Horizontal)
@@ -58,23 +63,26 @@ class SensorViewsContext(BaseContext):
 		self.layout.addWidget(content_widget)
 
 	def connectControls(self) -> None:
-		self.data.data_ready.connect(self._updateDataSources)
-		self.data.data_ready.connect(self._updateControls)
+		self.data['history'].data_ready.connect(self._updateDataSources)
+		self.data['history'].data_ready.connect(self._updateControls)
 		self.controls.time_slider.add_connect(self._updateDisplayedIndex)
 		self.controls.sensor_view_selectors.selected.connect(self.setViewActiveSensor)
 
 	def _validateDataType(self) -> None:
-		if self.data is not None and self.data.getType() != self.data_type:
-			console.sendErr(f"Error: history3D context has wrong data type: {self.data.getType()}")
-			console.sendErr(f"\t should be: {self.data_type}")
+		if self.data['history'] is not None and self.data['history'].getType() not in self.data_type:
+			console.sendErr(f"Error: history3D context has wrong data type: {self.data['history'].getType()}")
+			console.sendErr(f"\t should be one of: {self.data_type}")
+		if self.data['raycast_src'] is not None and self.data['raycast_src'].getType() not in self.data_type:
+			console.sendErr(f"Error: history3D context has wrong data type: {self.data['raycast_src'].getType()}")
+			console.sendErr(f"\t should be one of: {self.data_type}")
 
 	def _configureData(self) -> None:
 		pass
 
 	def _updateControls(self, *args, **kwargs) -> None:
-		self.controls.time_slider.setRange(self.data.getTimespan().start,
-									  		self.data.getTimespan().end,
-											len(self.data.getTimespan()))
+		self.controls.time_slider.setRange(self.data['history'].getTimespan().start,
+									  		self.data['history'].getTimespan().end,
+											len(self.data['history'].getTimespan()))
 		self.controls.time_slider.setValue(int(self.controls.time_slider.num_ticks/2))
 		self.controls.updateSensorViewLists()
 
@@ -85,9 +93,9 @@ class SensorViewsContext(BaseContext):
 		self._updateDisplayedIndex(self.controls.time_slider.slider.value())
 
 	def _updateDisplayedIndex(self, index:int) -> None:
-		if self.data is None:
-			logger.warning(f"model data is not set for context {self.config['name']}:{self}")
-			ValueError(f"model data is not set for context {self.config['name']}:{self}")
+		if self.data['history'] is None:
+			logger.warning(f"model history data is not set for context {self.config['name']}:{self}")
+			ValueError(f"model history data is not set for context {self.config['name']}:{self}")
 		self.canvas_wrapper.updateIndex(index)
 		self.canvas_wrapper.recomputeRedraw()
 
@@ -122,5 +130,5 @@ class Controls(BaseControls):
 		self.menubar = controls.Menubar(self.context.window, self.action_dict, context_name=self.context.config['name'])
 
 	def updateSensorViewLists(self):
-		sens_dict = self.context.data.getPrimaryConfig().serialiseAllSensors()
+		sens_dict = self.context.data['history'].getPrimaryConfig().serialiseAllSensors()
 		self.sensor_view_selectors.setSelectorLists(sens_dict)
