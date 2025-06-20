fixing array shapes such that ray index is the first axis; hold valid masking for rays hitting the earth into rayCastFromSensor

satplot/model/data_models/earth_raycast_data.py
@@ -112,30 +112,21 @@ class EarthRayCastData(BaseDataModel):
 		Returns:
 			[type]: [description]
 		'''
-		l = len(sens_rays_cf)
-		mid_idx = int(l/2)
+		num_rays = len(sens_rays_cf)
 		cf_norms = np.linalg.norm(sens_rays_cf[:,:3],axis=1)
-		# print(f'{cf_norms.max()=}')
 		sens_rays_eci = sens_eci_transform[:3,:3].dot(sens_rays_cf[:,:3].T).T
-		print(f'{sens_rays_eci.shape=},{sens_rays_eci[mid_idx]=}')
 		sens_rays_ecf = np.array(pymap3d.eci2ecef(sens_rays_eci[:,0],sens_rays_eci[:,1],sens_rays_eci[:,2],curr_dt)).T
-		print(f'{sens_rays_ecf.shape=},{sens_rays_ecf[mid_idx]=}')
 		norms = np.linalg.norm(sens_rays_eci,axis=1)
-		# print(f'{norms.max()=}')
 		pos_eci = sens_eci_transform[:3,3]
-		print(f'{pos_eci.shape=},{pos_eci=}')
 		pos_ecf = np.asarray(pymap3d.eci2ecef(pos_eci[0], pos_eci[1], pos_eci[2],curr_dt))
-		print(f'{pos_ecf.shape=},{pos_ecf=}')
 		cart_to_earth, valid = self._lineOfSightToSurface(pos_ecf, sens_rays_ecf)
-		print(f'{cart_to_earth.shape=}')
-		lats, lons = self._convertCartesianToEllipsoidGeodetic(cart_to_earth)
-		print(f'{lats.shape=}')
-		print(f'{lons.shape=}')
+		lats = np.zeros(num_rays)
+		lons = np.zeros(num_rays)
+		lats[valid], lons[valid] = self._convertCartesianToEllipsoidGeodetic(cart_to_earth[valid,:])
 		data = self.getPixelDataOnSphere(lats, lons)
 		rays_shape = sens_rays_cf.shape
-		full_img = np.zeros((rays_shape[0], 3))
-		full_img[valid] = data
-
+		full_img = np.zeros((num_rays, 3))
+		full_img[valid] = data[valid]
 		return full_img
 
 	def rayCastFromSensor2(self, sens_eci_transform:np.ndarray, sens_rays_cf:np.ndarray, curr_dt:dt.datetime) -> np.ndarray:
@@ -178,7 +169,7 @@ class EarthRayCastData(BaseDataModel):
 			lon (ndarray(dtype=float, ndim = N)): longitudes of input pointsq
 		'''
 
-		x, y, z = cart
+		x, y, z = cart.T
 		lon = np.degrees(np.arctan2(y,x))
 		inverse_flattening = 298.257223563 #wikipedia
 		f = 1.0 / inverse_flattening
@@ -253,7 +244,7 @@ class EarthRayCastData(BaseDataModel):
 		valid[d < 0] = False
 		valid[radical < 0] = False
 
-		return np.array([ x + d * u, y + d * v, z + d * w])[:,valid], valid
+		return np.array([ x + d * u, y + d * v, z + d * w]).T, valid
 
 	def prepSerialisation(self) -> dict[str, Any]:
 		state = {}

satplot/model/data_models/sphere_img_data.py
@@ -2,6 +2,7 @@ import logging
 import numpy as np
 import pathlib
 from PIL import Image
+import sys
 from typing import Any
 
 import satplot.model.geometry.spherical as spherical_geom
@@ -36,7 +37,7 @@ class SphereImageData(BaseDataModel):
 
 	def loadSource(self, running:threading.Flag):
 		im = Image.open(pathlib.Path(self.config['img_path']))
-		return np.array(im)/255
+		return np.array(im)
 
 	def storeArray(self, arr:np.ndarray) -> None:
 		logger.info(f"Finished loading image array data for {self.getConfigValue('body_name')}:" \
@@ -56,9 +57,11 @@ class SphereImageData(BaseDataModel):
 		if self.arr is None:
 			logger.error(f"{self.getConfigValue('body_name')}: SphereImage Data not loaded yet")
 			raise ValueError(f"{self.getConfigValue('body_name')}: SphereImage Data not loaded yet")
+		if lat.shape[0] == 0:
+			return np.ndarray((0,0,3))
 		res = self.getConfigValue('resolution')
 		lon = spherical_geom.wrapToCircleRangeDegrees(lon)
-		lat_pixel_coords = ((lat+90) / 180 * res[1]).astype(int)
+		lat_pixel_coords = ((90-lat) / 180 * res[1]).astype(int)
 		lon_pixel_coords = ((lon+180) / 360 * res[0]).astype(int)
 		return self.arr[lat_pixel_coords, lon_pixel_coords,:]
 