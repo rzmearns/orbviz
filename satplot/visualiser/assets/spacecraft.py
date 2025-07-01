6b8f3f14d6c24dec7bba1bebe9a37959e7a601cd
Robert Mearns <robert.mearns@unimelb.edu.au>
2025-07-01 21:13:19 +1000

    working over the horizon circle for a spacecraft

--- satplot/visualiser/assets/spacecraft.py
+++ satplot/visualiser/assets/spacecraft.py
@@ -1,6 +1,7 @@
 import logging
 import numpy as np
 from scipy.spatial.transform import Rotation
+import time
 from typing import Any
 from vispy import scene
 from vispy.scene.widgets.viewbox import ViewBox
@@ -8,6 +9,9 @@ from vispy.scene.widgets.viewbox import ViewBox
 import satplot.model.data_models.data_types as data_types
 import satplot.model.data_models.history_data as history_data
 import satplot.model.data_models.earth_raycast_data as earth_raycast_data
+import satplot.model.geometry.polygons as polygeom
+import satplot.model.geometry.spherical as spherical_geom
+import satplot.util.constants as c
 import satplot.visualiser.assets.sensors as sensors
 import satplot.visualiser.colours as colours
 import satplot.visualiser.assets.base_assets as base_assets
@@ -332,11 +336,17 @@ class Spacecraft2DAsset(base_assets.AbstractAsset):
 		self.data['strings'] = ['']
 		self.data['sens_suites'] = {}
 		self.data['coords'] = np.zeros((4,2))
+		self.data['eci_coords'] = np.zeros((4,2))
 		self.data['scaled_coords'] = np.zeros((4,2))
 		self.data['curr_index'] = 2
 		self.data['sc_config'] = None
 		self.data['history_src'] = None
 		self.data['raycast_src'] = None
+		self.data['oth_edge1'] = np.zeros((364,2))
+		# need to create a valid polygon for instantiation (but before valid data exists)
+		self.data['oth_edge1'][:363,0] = np.arange(0,363)
+		self.data['oth_edge1'][-1,0] = -1
+		self.data['oth_edge2'] = self.data['oth_edge1'].copy()
 
 	def setSource(self, *args, **kwargs) -> None:
 		# args[0] spacecraft configuration
@@ -389,6 +399,7 @@ class Spacecraft2DAsset(base_assets.AbstractAsset):
 			lat = ((first_sat_orbit.lat + 90) * self.data['vert_pixel_scale']).reshape(-1,1)
 			lon = ((first_sat_orbit.lon + 180) * self.data['horiz_pixel_scale']).reshape(-1,1)
 			self.data['scaled_coords'] = np.hstack((lon,lat))
+			self.data['eci_coords'] = first_sat_orbit.pos
 			logger.debug(f'Setting source:coordinates for {self}')
 		else:
 			console.sendErr('Orbit has no position data')
@@ -457,6 +468,23 @@ class Spacecraft2DAsset(base_assets.AbstractAsset):
 										size=self.opts['spacecraft_marker_size']['value'],
 										symbol='o')
 		self.visuals['marker'].order = -20
+		self.visuals['oth_circle1'] = scene.visuals.Polygon(self.data['oth_edge1'],
+															 color=colours.normaliseColour(self.opts['over_the_horizon_circle_colour']['value']),
+															 border_color=colours.normaliseColour(self.opts['over_the_horizon_circle_colour']['value']),
+															 border_width=1,
+															 parent=None)
+		self.visuals['oth_circle1'].opacity = self.opts['over_the_horizon_circle_alpha']['value']
+		self.visuals['oth_circle1'].order = 1
+		self.visuals['oth_circle1'].set_gl_state('translucent', depth_test=False)
+		self.visuals['oth_circle2'] = scene.visuals.Polygon(self.data['oth_edge2'],
+															 color=colours.normaliseColour(self.opts['over_the_horizon_circle_colour']['value']),
+															 border_color=colours.normaliseColour(self.opts['over_the_horizon_circle_colour']['value']),
+															 border_width=1,
+															 parent=None)
+		self.visuals['oth_circle2'].opacity = self.opts['over_the_horizon_circle_alpha']['value']
+		self.visuals['oth_circle2'].order = 1
+		self.visuals['oth_circle2'].set_gl_state('translucent', depth_test=False)
+
 
 	def getSensorSuiteByKey(self, sens_suite_key:str) -> sensors.SensorSuiteImageAsset:
 		# key = self.data['sc_config'].values()[sens_suite_id]
@@ -509,6 +537,24 @@ class Spacecraft2DAsset(base_assets.AbstractAsset):
 				self.data['curr_pos'] = orbit_data.pos[self.data['curr_index']].reshape(1,3)
 				self.data['curr_quat'] = quat
 				self._recomputeRedrawChildren(pos=orbit_data.pos[self.data['curr_index']].reshape(1,3), rotation=rotation)
+
+			# Over the horizon circle asset
+			self.data['oth_edge1'], self.data['oth_edge2'], split = self.calcOTHCircle()
+			verts, faces = polygeom.polygonTriangulate(self.data['oth_edge1'])
+			self.visuals['oth_circle1']._mesh.set_data(vertices=verts, faces=faces)
+			self.visuals['oth_circle1'].pos=self.data['oth_edge1']
+			verts, faces = polygeom.polygonTriangulate(self.data['oth_edge2'])
+			self.visuals['oth_circle2']._mesh.set_data(vertices=verts, faces=faces)
+			self.visuals['oth_circle2'].pos=self.data['oth_edge2']
+
+			if split:
+				self.visuals['oth_circle1'].opacity = self.opts['over_the_horizon_circle_alpha']['value']
+				self.visuals['oth_circle2'].opacity = self.opts['over_the_horizon_circle_alpha']['value']
+				# self.visuals['oth_circle1'].opacity = 1
+				# self.visuals['oth_circle2'].opacity = 1
+			else:
+				self.visuals['oth_circle1'].opacity = self.opts['over_the_horizon_circle_alpha']['value']/2
+				self.visuals['oth_circle2'].opacity = self.opts['over_the_horizon_circle_alpha']['value']/2
 			self._clearStaleFlag()
 
 	def getScreenMouseOverInfo(self) -> dict[str, Any]:
@@ -541,6 +587,24 @@ class Spacecraft2DAsset(base_assets.AbstractAsset):
 												'static': True,
 												'callback': self.setOrbitalMarkerSize,
 												'widget_data':None}
+		self._dflt_opts['plot_over_the_horizon_circle'] = {'value': True,
+												'type': 'boolean',
+												'help': '',
+												'static': True,
+												'callback': self.setOTHCircleVisibility,
+											'widget_data': None}
+		self._dflt_opts['over_the_horizon_circle_colour'] = {'value': (255,234,0),
+												'type': 'colour',
+												'help': '',
+												'static': True,
+												'callback': self.setOTHCircleColour,
+											'widget_data': None}
+		self._dflt_opts['over_the_horizon_circle_alpha'] = {'value': 0.4,
+												'type': 'fraction',
+												'help': '',
+												'static': True,
+												'callback': self.setOTHCircleAlpha,
+												'widget_data': None}
 		self.opts = self._dflt_opts.copy()
 
 	#----- OPTIONS CALLBACKS -----#
@@ -551,6 +615,24 @@ class Spacecraft2DAsset(base_assets.AbstractAsset):
 	def setOrbitalMarkerSize(self, value:int) -> None:
 		self.opts['spacecraft_marker_size']['value'] = value
 		self._updateMarkers()
+
+	def setOTHCircleAlpha(self, alpha):
+		# Takes a little while to take effect.
+		logger.debug(f"Changing terminator alpha {self.opts['over_the_horizon_circle_alpha']['value']} -> {alpha}")
+		self.opts['over_the_horizon_circle_alpha']['value'] = alpha
+		self.visuals['oth_circle1'].opacity = self.opts['over_the_horizon_circle_alpha']['value']
+		self.visuals['oth_circle2'].opacity = self.opts['over_the_horizon_circle_alpha']['value']
+
+	def setOTHCircleColour(self, new_colour):
+		logger.debug(f"Changing terminator colour {self.opts['over_the_horizon_circle_colour']['value']} -> {new_colour}")
+		self.opts['over_the_horizon_circle_colour']['value'] = new_colour
+		self.visuals['oth_circle1'].color = self.opts['over_the_horizon_circle_colour']['value']
+		self.visuals['oth_circle2'].color = self.opts['over_the_horizon_circle_colour']['value']
+
+	def setOTHCircleVisibility(self, state):
+		self.opts['plot_over_the_horizon_circle']['value'] = state
+		self.visuals['oth_circle1'].visible = self.opts['plot_over_the_horizon_circle']['value']
+		self.visuals['oth_circle2'].visible = self.opts['plot_over_the_horizon_circle']['value']
 
 	def setAllSensorSuitesVisibility(self, state:bool) -> None:
 		for key, asset in self.assets.items():
@@ -604,6 +686,27 @@ class Spacecraft2DAsset(base_assets.AbstractAsset):
 				self.opts[f'plot_sensor_suite_{suite_name}']['widget_data']['mark_for_removal'] = True
 			self.assets[f'sensor_suite_{suite_name}'].removePlotOptions()
 			del(self.opts[f'plot_sensor_suite_{suite_name}'])
+
+	def calcOTHCircle(self):
+		pos = self.data['coords'][self.data['curr_index']]
+		eci_pos = self.data['eci_coords'][self.data['curr_index']]
+		alt = np.linalg.norm(eci_pos)
+		phi = np.rad2deg(np.arccos(c.R_EARTH/(alt)))
+		lats, lons1, lons2 = spherical_geom.genSmallCircleCenterSubtendedAngle(phi*2, pos[1], pos[0])
+		circle1, circle2 = spherical_geom.splitSmallCirclePatch2(pos[0], pos[1], lats, lons1, lons2)
+
+		if np.all(circle1 == circle2):
+			split = False
+		else:
+			split = True
+
+		return self._scale(circle1), self._scale(circle2), split
+
+	def _scale(self, coords):
+		out_arr = coords.copy()
+		out_arr[:,0] = (out_arr[:,0] + 180) * self.data['horiz_pixel_scale']
+		out_arr[:,1] = (out_arr[:,1] + 90) * self.data['vert_pixel_scale']
+		return out_arr
 
 class SpacecraftViewsAsset(base_assets.AbstractAsset):
 	def __init__(self, name:str|None=None, v_parent:ViewBox|None=None):
