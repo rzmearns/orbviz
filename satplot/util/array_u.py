2299131099351796d01c11b3b736b534052c925a
Robert Mearns <robert.mearns@unimelb.edu.au>
2025-08-01 21:23:03 +1000

    removing unused array_util functions

--- satplot/util/array_u.py
+++ satplot/util/array_u.py
@@ -52,20 +52,6 @@ def uniqueRowsOrdered(arr:nptyping.NDArray, to_decimal:int=8) -> nptyping.NDArra
 
 	return arr[sorted_unique_idxs,:]
 
-def arrayOverlap(a:nptyping.NDArray, b:nptyping.NDArray) -> nptyping.NDArray:
-	'''[summary]
-	
-	[description]
-	
-	Parameters
-	----------
-	a : {[type]}
-		[description]
-	b : {[type]}
-		[description]
-	'''
-	return np.array([x for x in set(tuple(x) for x in a) & set(tuple(x) for x in b)])
-
 def interpNans(y, x=None):
 	if x is None:
 		x = np.arange(len(y))
@@ -109,35 +95,3 @@ def nonMonotonicInterpNans(x, y):
 
 	out_y[nans] = new_ys
 	return out_y
-
-
-def findRows(subarr:nptyping.NDArray, arr:nptyping.NDArray) -> list[int]:
-	'''Finds indices in an array row-wise
-	
-	Finds indices of each row of `subarr` in `arr`
-	
-	Parameters
-	----------
-	subarr : {(n,3) ndarray}
-		array to find indices of row-wise
-	arr : {(n,3) ndarray}
-		search array
-	
-	Returns
-	-------
-	[list]
-		list of indices
-	'''
-	num_rows=len(subarr)
-	indices=[]
-	for row in subarr:
-		indices.append(np.where((arr == row).all(axis=1))[0][0])
-
-	return indices
-
-def nonUnique(arr:nptyping.NDArray) -> list[nptyping.NDArray]:
-	idx_sort=np.argsort(arr)
-	sorted_arr=arr[idx_sort]
-	vals,idx_start,count=np.unique(sorted_arr,return_counts=True,return_index=True)
-
-	return np.split(idx_sort,idx_start[1:])
