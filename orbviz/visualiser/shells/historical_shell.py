ecc8c1f6dad4130e1137af443f7dfedbea800f3b
Robert Mearns <robert.mearns@unimelb.edu.au>
2025-09-07 17:50:00 +0100

    only create timeseries once for first propagation, otherwise update the existing timeseries

--- orbviz/visualiser/shells/historical_shell.py
+++ orbviz/visualiser/shells/historical_shell.py
@@ -17,6 +17,10 @@ from orbviz.visualiser.shells import base_shell
 
 logger = logging.getLogger(__name__)
 
+# This flag is just for prototyping
+# TODO: remove
+timeseries_created = False
+
 class HistoricalShell(base_shell.BaseShell):
 	def __init__(self, parent_window:QtWidgets.QMainWindow, toolbars:dict[str, controls.Toolbar],
 															menubars:dict[str, controls.Menubar],
@@ -84,8 +88,22 @@ class HistoricalShell(base_shell.BaseShell):
 		self.context_tab_stack.setCurrentIndex(1)
 
 	def _onDataReadyCreateTS(self) -> None:
-		console.send('Building Timeseries from History Data Model')
-		for key in ['sun', 'moon','orbits.pos', 'orbits.pos_ecef','orbits.vel','orbits.vel_ecef','orbits.lat','orbits.lon','orbits.alt','orbits.eclipse']:
-			if key not in self.timeseries_data.keys():
+		# TODO: turn this into an update function, always called when the data is ready.
+		# TODO: move this into the _procDataUpdated/_updateDataSources method of timeseries context
+		global timeseries_created
+		if not timeseries_created:
+			console.send('Building Timeseries from History Data Model')
+			for key in ['sun', 'moon','orbits.pos', 'orbits.pos_ecef','orbits.vel','orbits.vel_ecef','orbits.lat','orbits.lon','orbits.alt','orbits.eclipse']:
 				for ts_key, ts in timeseries.createTimeSeriesFromDataModel(self.data['history'], key).items():
 					self.timeseries_data[ts_key] = ts
+			timeseries_created = True
+		else:
+			for ts_key, ts in self.timeseries_data.items():
+				ts.update()
+
+		self.contexts_dict['timeseries-history'].canvas_wrapper.modelUpdated()
+
+		# print(f"\t{hex(id(self.timeseries_data['orbits.alt'].ordinate))=}")
+		# print(f"\t\t{self.timeseries_data['orbits.alt'].ordinate.max()=}")
+		# print(f"\t\t{self.timeseries_data['orbits.alt'].ordinate.min()=}")
+		# print(f"\t{self.timeseries_data['sun_x']=}")
