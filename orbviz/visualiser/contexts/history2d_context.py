d04040df12fc7ee7a2d75a4cf677c2ca91c065eb
Robert Mearns <robert.mearns@unimelb.edu.au>
2025-11-20 12:51:52 +1100

    removing overriding saveGIF implementation in history2d_context (forgot to remove in earlier commit)

--- orbviz/visualiser/contexts/history2d_context.py
+++ orbviz/visualiser/contexts/history2d_context.py
@@ -144,42 +144,6 @@ class History2DContext(base.BaseContext):
 	def _centerCameraEarth(self) -> None:
 		self.canvas_wrapper.centerCameraEarth()
 
-	def saveGif(self, gif_config):
-		# TODO: need to lockout controls
-		console.send('Starting GIF saving, please do not touch the controls.')
-		if gif_config.loop:
-			num_loops = 0
-		else:
-			num_loops = 1
-
-		writer = imageio.get_writer(gif_config.file_path, loop=num_loops)
-
-		# calculate viewport of just the canvas
-		geom = self.canvas_wrapper.canvas.native.geometry()
-		ratio = self.canvas_wrapper.canvas.native.devicePixelRatio()
-		geom = (geom.x(), geom.y(), geom.width(), geom.height())
-		new_pos = self.canvas_wrapper.canvas.native.mapTo(self.window, QtCore.QPoint(0, 0))
-		new_y = self.window.height() - (new_pos.y() + geom[3])
-		viewport = (new_pos.x() * ratio, new_y * ratio, geom[2] * ratio, geom[3] * ratio)
-
-		for ii in range(gif_config.num_steps):
-			curr_timespan_idx = gif_config.start_idx + ii
-			self.controls.time_slider.setValue(curr_timespan_idx)
-			app.process_events()
-
-			im = _screenshot(viewport=viewport)
-			writer.append_data(im)
-			# use this to print to console on last iteration, otherwise thread doesn't get serviced until after writer closes
-			if ii==gif_config.num_steps-2:
-				console.send("Writing file. Please wait...")
-				app.process_events()
-
-		writer.close()
-		# reset to pre-fig state
-		self.controls.time_slider.setValue(gif_config.start_idx)
-		del(self._gif_dialog)
-		console.send(f"Saved {self.config['name']} GIF to {gif_config.file_path}")
-
 	def setupGIFDialog(self):
 		# add check that timespan is not None
 		cam_config = cam_utility_types.RestrictedPanZoomCameraAdjustment()
